4ed8cfe3b95de628d80b49927d38704fb4063972
ihorshnaider <ihor_shnaider@outlook.com>
2024-08-12 20:18:26 +0300

    Use custom adapter extension

--- main/index.ts
+++ main/index.ts
@@ -1,7 +1,33 @@
-import { Context, HttpRequest } from '@azure/functions';
-import { AzureHttpAdapter } from '@nestjs/azure-func-http';
-import { createApp } from '../src/main.azure';
+import { Context, HttpRequest } from "@azure/functions";
+import { AzureHttpAdapter, AzureRequest } from "@nestjs/azure-func-http";
+import { createApp } from "../src/main.azure";
+import { INestApplication } from "@nestjs/common";
 
-export default function(context: Context, req: HttpRequest): void {
-  AzureHttpAdapter.handle(createApp, context, req);
+function createPsuedoApp(
+  createApp: () => Promise<INestApplication>
+): () => Promise<any> {
+  return async (): Promise<any> => {
+    const app = await createApp();
+    const psuedoApp = {
+      getHttpAdapter: () => {
+        return {
+          getInstance: () => {
+            return (req: AzureRequest, res: any) => {
+              const done = req.context.done;
+              req.context.done = (err?: string | Error, result?: any) => {
+                res.writeHead();
+                done(err, result);
+              };
+              app.getHttpAdapter().getInstance()(req, res);
+            };
+          },
+        };
+      },
+    };
+    return psuedoApp;
+  };
+}
+
+export default function (context: Context, req: HttpRequest): void {
+  AzureHttpAdapter.handle(createPsuedoApp(createApp), context, req);
 }
