ce8efb8b85e4cc5df06e8d70713de581083244b5
ihorshnaider <ihor_shnaider@outlook.com>
2024-08-12 20:29:43 +0300

    Refactor custom adapter

--- main/index.ts
+++ main/index.ts
@@ -3,31 +3,33 @@ import { AzureHttpAdapter, AzureRequest } from "@nestjs/azure-func-http";
 import { createApp } from "../src/main.azure";
 import { INestApplication } from "@nestjs/common";
 
-function createPsuedoApp(
+function createPseudoApp(
   createApp: () => Promise<INestApplication>
-): () => Promise<any> {
-  return async (): Promise<any> => {
+): () => Promise<INestApplication> {
+  return async (): Promise<INestApplication> => {
     const app = await createApp();
-    const psuedoApp = {
-      getHttpAdapter: () => {
-        return {
-          getInstance: () => {
-            return (req: AzureRequest, res: any) => {
-              const done = req.context.done;
-              req.context.done = (err?: string | Error, result?: any) => {
-                res.writeHead();
-                done(err, result);
-              };
-              app.getHttpAdapter().getInstance()(req, res);
-            };
-          },
-        };
-      },
+    
+    const pseudoApp: INestApplication = {
+      ...app,
+      getHttpAdapter: () => ({
+        ...app.getHttpAdapter(),
+        getInstance: () => (req: AzureRequest, res: any) => {
+          const originalDone = req.context.done;
+          req.context.done = (err?: string | Error, result?: any) => {
+            if (typeof res.writeHead === 'function') {
+              res.writeHead();
+            }
+            originalDone(err, result);
+          };
+          app.getHttpAdapter().getInstance()(req, res);
+        },
+      }),
     };
-    return psuedoApp;
+
+    return pseudoApp;
   };
 }
 
 export default function (context: Context, req: HttpRequest): void {
-  AzureHttpAdapter.handle(createPsuedoApp(createApp), context, req);
+  AzureHttpAdapter.handle(createPseudoApp(createApp), context, req);
 }
